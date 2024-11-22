a2da8bdbf7e29d8c7ef1e68898fa6994a7b2863a
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2024-11-22 11:13:41 +0530

    fixes some issues in userControler

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -50,6 +50,7 @@ export const createUser = async (req: Request, res: Response) => {
 
     res.status(201).json({
       message: "User created successfully",
+      //@ts-ignore
       user: { id: user.id, name: user.name, email: user.email, role: user.role },
     });
   } catch (error) {
@@ -70,6 +71,7 @@ export const updateUser = async (req: Request, res: Response) => {
 
     res.status(200).json({
       message: "User updated successfully",
+      //@ts-ignore
       user: { id: user.id, name: user.name, email: user.email, role: user.role },
     });
   } catch (error) {
