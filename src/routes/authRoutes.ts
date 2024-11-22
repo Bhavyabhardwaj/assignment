aa903b533a5c43756bb5c4a5e6fef218f248fb9e
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2024-11-22 15:44:53 +0530

    complete backend

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,8 +2,11 @@ import express from "express";
 import { userSignup, adminSignup, login } from "../controllers/authController";
 const router = express.Router();
 
+//@ts-ignore
 router.post("/signup/user", userSignup); // User Signup
+//@ts-ignore
 router.post("/signup/admin", adminSignup); // Admin Signup
+//@ts-ignore
 router.post("/login", login); // Login for both Admin and Users
 
 export default router;
--- src/routes/roleRoutes.ts
+++ src/routes/roleRoutes.ts
@@ -12,12 +12,16 @@ import {
 const router = express.Router();
 
 // Admin routes to manage roles
+//@ts-ignore
 router.get("/", authenticate, isAdmin, getRoles); // Get all roles
+//@ts-ignore
 router.post("/", authenticate, isAdmin, createRole); // Create a new role
+//@ts-ignore
 router.patch("/:id", authenticate, isAdmin, updateRole); // Update a role
+//@ts-ignore
 router.delete("/:id", authenticate, isAdmin, deleteRole); // Delete a role
-
 // Admin route to assign permissions to a role
+//@ts-ignore
 router.post("/:roleId/permissions", authenticate, isAdmin, assignPermissionsToRole); // Assign permissions to a role
 
 export default router;
